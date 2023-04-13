71ea323a752e32fa78003301c5fcd905d0137c1f
JordaoGustavo <jordaogustavo3@gmail.com>
2023-04-13 20:53:55 -0300

    change docker compose file

--- src/services/TheBrain/src/index.ts
+++ src/services/TheBrain/src/index.ts
@@ -3,11 +3,6 @@ import SocketMessageProducer from '@infrastructure/messageProducer/SocketMessage
 import NetSocketConnection from '@infrastructure/Socket/NetSocketConnection';
 import NetClientSocketConnection from '@infrastructure/Socket/NetClientSocketConnection';
 
-
-
-
-  const listener = new IncidentMessagePostgreesListener(new SocketMessageProducer(new NetSocketConnection()));
-  listener.listen();
-  const client = new NetClientSocketConnection();
-
-
+const listener = new IncidentMessagePostgreesListener(new SocketMessageProducer(new NetSocketConnection()));
+listener.listen();
+const client = new NetClientSocketConnection();
